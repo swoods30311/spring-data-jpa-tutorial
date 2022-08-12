269c7ba27f99f7c5f43d93af97d3b6e20a5b43de
Sandy Woods <sandywoods@Sandys-Air-2.attlocal.net>
2022-08-12 02:25:52 -0400

    Mapping Entities w/ DB

---
+++ src/main/java/com/dailycodebuffer/spring/data/jpa/tutorial/entity/entity/Student.java
@@ -0,0 +1,26 @@
+package com.dailycodebuffer.spring.data.jpa.tutorial.entity.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Student {
+
+    @Id
+    private Long studentId;
+    private String firstName;
+    private String lastName;
+    private String emailId;
+    private String guardianName;
+    private String guardianEmail;
+    private String guardianMobile;
+}
