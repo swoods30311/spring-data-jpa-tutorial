d59f0d2b1589ca6ea5abc1bef57fa24eb1b778de
Sandy Woods <sandywoods@Sandys-Air-2.attlocal.net>
2022-08-17 18:32:20 -0400

    Creating JPA Repositories & methods

--- src/test/java/com/dailycodebuffer/spring/data/jpa/tutorial/entity/repository/StudentRepositoryTest.java
+++ src/test/java/com/dailycodebuffer/spring/data/jpa/tutorial/entity/repository/StudentRepositoryTest.java
@@ -55,4 +55,29 @@ class StudentRepositoryTest {
 
         System.out.println("studentList = " + studentList);
     }
+
+    @Test
+    public void printStudentByFirstName() {
+
+        List<Student> students =
+                studentRepository.findByFirstName("shivam");
+
+        System.out.println("students = " + students);
+    }
+
+    @Test
+    public void printStudentByFirstNameContaining() {
+
+        List<Student> students =
+                studentRepository.findByFirstNameContaining("sh");
+
+        System.out.println("students = " + students);
+    }
+
+    @Test
+    public void printStudentBasedOnGuardianName(){
+        List<Student> students =
+                studentRepository.findByGuardianName("Nikhil");
+        System.out.println("students = " + students);
+    }
 }
