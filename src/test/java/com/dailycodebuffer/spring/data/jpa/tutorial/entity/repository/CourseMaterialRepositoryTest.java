938147044a4b4b4cfc37a97b6d9a38335217cc6c
Sandy Woods <sandywoods@Sandys-Air-2.attlocal.net>
2022-08-20 19:59:34 -0400

    JPA @OneToMany Relationship

--- src/test/java/com/dailycodebuffer/spring/data/jpa/tutorial/entity/repository/CourseMaterialRepositoryTest.java
+++ src/test/java/com/dailycodebuffer/spring/data/jpa/tutorial/entity/repository/CourseMaterialRepositoryTest.java
@@ -20,13 +20,13 @@ class CourseMaterialRepositoryTest {
     public void SaveCourseMaterial() {
         Course course =
                 Course.builder()
-                .title("DSA")
+                .title(".net")
                 .credit(6)
                 .build();
 
         CourseMaterial courseMaterial =
                 CourseMaterial.builder()
-                .url("www.google.com")
+                .url("www.dailycodebuffer.com")
                 .course(course)
                 .build();
 
