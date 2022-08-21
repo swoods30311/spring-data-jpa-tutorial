33a138415ab9e775cc2293425c9fd04d24c9585c
Sandy Woods <sandywoods@Sandys-Air-2.attlocal.net>
2022-08-21 00:30:08 -0400

    Paging & Sorting

--- src/main/java/com/dailycodebuffer/spring/data/jpa/tutorial/entity/repository/CourseRepository.java
+++ src/main/java/com/dailycodebuffer/spring/data/jpa/tutorial/entity/repository/CourseRepository.java
@@ -2,6 +2,9 @@ package com.dailycodebuffer.spring.data.jpa.tutorial.entity.repository;
 
 import com.dailycodebuffer.spring.data.jpa.tutorial.entity.entity.Course;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,5 +12,7 @@ import java.util.List;
 
 @Repository
 public interface CourseRepository extends JpaRepository<Course,Long> {
+
+    Page<Course> findByTitleContaining(String title, Pageable pageRequest);
 
 }
--- src/test/java/com/dailycodebuffer/spring/data/jpa/tutorial/entity/repository/CourseRepositoryTest.java
+++ src/test/java/com/dailycodebuffer/spring/data/jpa/tutorial/entity/repository/CourseRepositoryTest.java
@@ -1,10 +1,15 @@
 package com.dailycodebuffer.spring.data.jpa.tutorial.entity.repository;
 
+import com.dailycodebuffer.spring.data.jpa.tutorial.entity.entity.Student;
 import com.dailycodebuffer.spring.data.jpa.tutorial.entity.entity.Course;
 import com.dailycodebuffer.spring.data.jpa.tutorial.entity.entity.Teacher;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+
 import java.util.List;
 
 @SpringBootTest
@@ -37,6 +42,71 @@ class CourseRepositoryTest {
 
         courseRepository.save(course);
     }
+    @Test
+    public void findAllPagination(){
+        Pageable firstPagewithThreeRecords =
+                PageRequest.of(0, 3);
+        Pageable secondPageWithTwoRecords =
+                PageRequest.of(1,2);
 
+        List<Course> courses =
+                courseRepository.findAll(secondPageWithTwoRecords)
+                        .getContent();
+        long totalElements =
+                courseRepository.findAll(secondPageWithTwoRecords)
+                        .getTotalElements();
 
+        long totalPages =
+                courseRepository.findAll(secondPageWithTwoRecords)
+                        .getTotalPages();
+
+        System.out.println("totalPages = " + totalPages);
+
+        System.out.println("totalElements = " + totalElements);
+
+        System.out.println("courses = " + courses);
+    }
+
+    @Test
+    public void findAllSorting() {
+        Pageable sortByTitle =
+                PageRequest.of(
+                        0,
+                        2,
+                        Sort.by("title")
+                );
+        Pageable sortByCreditDesc =
+                PageRequest.of(
+                        0,
+                        2,
+                        Sort.by("credit").descending()
+                );
+
+        Pageable sortByTitleAndCreditDesc =
+                PageRequest.of(
+                        0,
+                        2,
+                        Sort.by("title")
+                                .descending()
+                                .and(Sort.by("credit"))
+                );
+
+        List<Course> courses
+                = courseRepository.findAll(sortByTitle).getContent();
+
+        System.out.println("courses = " + courses);
+    }
+
+    @Test
+    public void printfindByTitleContaining() {
+        Pageable firstPageTenRecords =
+                PageRequest.of(0,10);
+
+        List<Course> courses =
+                courseRepository.findByTitleContaining(
+                        "D",
+                        firstPageTenRecords).getContent();
+
+        System.out.println("courses = " + courses);
+    }
 }
